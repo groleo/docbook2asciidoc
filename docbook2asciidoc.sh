08df675110b3c160f23f758d22c6faeb05d12113
Adrian Negreanu <adrian.m.negreanu@intel.com>
2014-11-02 11:33:31 +0200

    add proxy parameters and other fixes

--- docbook2asciidoc.sh
+++ docbook2asciidoc.sh
@@ -1,6 +1,8 @@
-#!/bin/sh
+#!/bin/bash
 
 REQUIRED_BASH_VERSION=3.0.0
+PROXY_HOST=
+PROXY_PORT=
 
 if [[ $BASH_VERSION < $REQUIRED_BASH_VERSION ]]; then
   echo "You must use Bash version 3 or newer to run this script"
@@ -9,22 +11,24 @@ fi
 
 DIR=$(cd -P -- "$(dirname -- "$0")" && pwd -P)
 
-convert() 
+convert()
 {
-  CMD="find $SCAN_DIR -name \*.$EXT"
+  CMD="find $SCAN_DIR -name *.$EXT"
   if [ $RECURSE == "0" ]; then
-    CMD="$CMD -maxdepth 1"
+    CMD="find $SCAN_DIR -maxdepth 2 -name *.$EXT"
   fi
-  xmls=`eval $CMD`
-
   mkdir -p $OUT_DIR
 
-  for xml in $xmls
-  do
-    output_filename=${xml/.$EXT/.$OEXT}
-    echo "Processing $xml -> $output_filename"    
-    CMD="java -jar $DIR/saxon9he.jar -s $xml -o $output_filename $DIR/d2a.xsl"
-    $CMD
+  for xml in `$CMD` ; do
+    output_filename=$OUT_DIR/`basename ${xml/.$EXT/.$OEXT}`
+    echo "Processing $xml -> $output_filename"
+    if [ -n "$PROXY_HOST" -a -n "$PROXY_PORT" ]; then
+      PROXY="-Dhttp.proxyHost=$PROXY_HOST -Dhttp.proxyPort=$PROXY_PORT"
+    fi
+    java $PROXY -jar $DIR/saxon9he.jar -s $xml -o $output_filename $DIR/d2a.xsl
+    fname=$(basename $xml)
+    mv $OUT_DIR/book-docinfo.xml $OUT_DIR/${fname%.*}-docinfo.xml
+    echo
   done
 }
 
@@ -36,13 +40,24 @@ usage: $0 options
 This script allows primitive batching of docbook to asciidoc conversion
 
 OPTIONS:
-   -s      Source directory to scan for files, by default the working directory
-   -x      Extension of files to convert, by default 'xml'
-   -o      Output extension, by default 'asciidoc'
-   -r      Enable recusive scanning, by default the scan is not recursive
+   -s      Source directory to scan for files [default:\$PWD]
+   -x      Extension of files to convert [default:'xml']
+   -o      Output extension [default:'asciidoc']
+   -O      Output directory [default:'output']
+   -r      Enable recusive scanning [default:not recursive]
+   -H      proxy Host
+   -P      proxy Port
+
    -h      Shows this message
 EOF
 }
+
+trap 'exit 0' SIGINT
+
+if [ $# -eq 0 ]; then
+  usage
+  exit 0
+fi
 
 SCAN_DIR=`pwd`
 RECURSE="0"
@@ -50,7 +65,7 @@ EXT="xml"
 OEXT="asciidoc"
 OUT_DIR="output"
 
-while getopts “hrx:o:s:” OPTION
+while getopts “hrx:o:s:O:H:P:” OPTION
 
 do
      case $OPTION in
@@ -70,6 +85,15 @@ do
          o)
              OEXT=$OPTARG
              ;;
+         O)
+            OUT_DIR=$OPTARG
+            ;;
+         H)
+            PROXY_HOST=$OPTARG
+            ;;
+         P)
+            PROXY_PORT=$OPTARG
+           ;;
          [?])
              usage
              exit
